465cc00af2c5aaa94748c25a6b4a6f8f226fb60d
nachomonllor <nachomonllorc@gmail.com>
2023-02-11 23:57:42 -0300

    print_next_greater_freq firma

---
+++ NextElementWithGreaterFrequency/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextElementWithGreaterFrequency
+{
+    class Program
+    {
+
+        static int[] print_next_greater_freq(int arr[], int n)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
