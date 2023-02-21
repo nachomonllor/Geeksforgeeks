8882c24f133bf3bf6243316138b1a1b70b749762
nachomonllor <nachomonllorc@gmail.com>
2023-02-21 06:57:00 -0300

    CountNumberOfFreeCell

---
+++ CountNumberOfFreeCell/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountNumberOfFreeCell
+{
+    class Program
+    {
+
+        static long[] countZero(int N, int K, int[][] arr)
+        {
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int fila = arr[i][0];
+                int col = arr[i][1];
+
+
+
+            }
+
+            return new long[0];
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+}
